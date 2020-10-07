258547f3704bd186fd6f99ad1bbce1bf54d0725a
Michael-Lee-1994 <Michael.Lee.1994.Flatiron@gmail.com>
2020-10-06 17:03:33 -0700

    finished lab

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,39 @@
+class Book   
+    def initalize(title)
+        @title = title
+    end
 
+    def title
+        @title
+    end
+
+    def author=(author)
+        @author
+    end
+
+    def author
+        @author
+    end
+
+    def page_count=(page_count)
+        @page_count
+    end
+
+    def page_count
+        @page_count
+    end
+
+    def genre=(genre)
+        @genre
+    end
+
+    def genre
+        @genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,51 @@
 # Make your shoe class here!
+class Shoe
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def brand
+        @brand = brand
+    end
+
+    def color=(color)
+        @color = color
+    end
+
+    def color
+        @color = color
+    end
+
+    def size=(size)
+        @size = size
+    end
+
+    def size
+        @size = size
+    end
+
+    def material=(material)
+        @material = material
+    end
+
+    def material
+        @material = material
+    end
+
+    def condition=(conditon)
+        @condition = condition
+    end
+
+    def condition
+        @condition = condition
+    end
+
+    def cobble
+        if @condition == "old" 
+            @condition = "new"
+            self.condition = ("new")
+        end
+        puts "Your shoe is as good as new!"   
+    end
+end
